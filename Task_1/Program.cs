2c3de6b71b078c002e93fd9543906f3f97731a94
Иван <polushinv@gmail.com>
2022-06-16 17:25:05 +0300

    ДЗ-3 готово

---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+int promt(string message)
+{
+    System.Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int a = promt("введите число: ");
+int b = a;
+var col = new List<int>();
+while (b > 0)
+{
+    col.Add(b % 10);
+    b = b / 10;
+}
+b = 0;
+col.Reverse();
+for (int mcol = 0; mcol < col.Count; mcol++)
+    b = b + col[mcol] * (int)Math.Pow(10, mcol);
+if (a == b)
+    Console.WriteLine("Данное число является палидромом");
+else
+    Console.WriteLine("Данное число не является палидромом");
+
+
---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+int promt(string message)
+{
+    System.Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int x1 = promt("Введите число: ");
+int y1 = promt("Введите число: ");
+int z1 = promt("Введите число: ");
+int x2 = promt("Введите число: ");
+int y2 = promt("Введите число: ");
+int z2 = promt("Введите число: ");
+int a = x2 - x1;
+int b = y2 - y1;
+int c = z2 - z1;
+double len = Math.Sqrt(a * a + b * b + c * c);
+System.Console.WriteLine("Расстояние между ними в 3D пространстве = " + len);
